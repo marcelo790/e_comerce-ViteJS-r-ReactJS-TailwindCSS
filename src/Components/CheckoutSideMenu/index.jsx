01b18fc20ab2c12e30b93413e8f4cc6e01681c59
marcelo790 <marcelocastano95@gmail.com>
2023-10-12 11:58:25 -0400

    Tienda en Linea - Desarrollado con ReactJS, Vite.JS y Tailwindcss

---
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -0,0 +1,73 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import './styles.css'
+import { ShoppingCardContext } from '../../Context';
+import { useContext } from 'react';
+import OrderCard from '../../Components/OrderCard';
+import { totalPrice } from '../../utils';
+import { Link } from 'react-router-dom';
+
+const CheckoutSideMenu = () => {
+    const context = useContext(ShoppingCardContext);
+
+    const handleDelete = (id) =>{
+        const filteredProducts = context.cartProducts.filter(product => product.id != id);
+        context.setCartProducts(filteredProducts)
+    }
+
+    const handleCheckout = () =>{
+        const orderToAdd = {
+            date: '01-02-2023',
+            products: context.cartProducts,
+            totalProducts: context.cartProducts.length,
+            totalPrice: totalPrice(context.cartProducts)
+        }
+        context.setOrder([...context.order, orderToAdd]);
+        context.setCartProducts([]);
+        context.setSearchByTitle(null);
+    }
+
+    return(
+        <aside 
+        className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>My Order</h2>
+                <div>
+                    <XMarkIcon 
+                        className="h-6 w-6 text-black cursor-pointer"
+                        onClick={() => context.closeCheckoutSideMenu()}
+                    ></XMarkIcon>
+                </div>
+            </div>
+            <div className='px-6 overflow-y-scroll flex-1'>
+            {
+                context.cartProducts.map(product => (
+                    <OrderCard 
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        imageUrl={product.images}
+                        price={product.price}
+                        handleDelete={handleDelete}
+                    />
+                ))
+            } 
+            </div>
+            <div className='px-6 mb-6'>
+                <p className='flex justify-between items-center mb-2'>
+                    <span className='font-light'>Total: </span>
+                    <span className='font-medium text-2xl'>$ {totalPrice(context.cartProducts)}</span>
+                </p>
+                <Link to='/my-orders/last'>
+                    <button
+                        className='w-full bg-black py-3 text-white rounded'
+                        onClick={() => handleCheckout()}
+                    >Checkout
+                    </button>
+                </Link>
+                
+            </div>           
+        </aside>
+    )
+}
+
+export default CheckoutSideMenu;
